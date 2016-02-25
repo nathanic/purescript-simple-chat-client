513587baca85238bf14c998f036d97766c07580a
Nathan Stien <nathanism@gmail.com>
2016-02-25 00:45:54 -0600

    various cleanuppance

--- src/Main.purs
+++ src/Main.purs
@@ -6,32 +6,25 @@ import Control.Alt ((<|>))
 import Control.Bind ((=<<))
 import Control.Monad (when)
 import Control.Monad.Rec.Class (forever)
-import Control.Monad.Aff (Aff(),launchAff,runAff,forkAff)
-import Control.Monad.Aff.AVar (AVar(),AVAR(),makeVar,putVar,takeVar)
--- import Control.Monad.Aff.Console (log)
+import Control.Monad.Aff (Aff(), launchAff, runAff)
+import Control.Monad.Aff.AVar (AVar(), AVAR(), makeVar, putVar, takeVar)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE(),log)
-import Control.Monad.Eff.Exception (EXCEPTION(),throwException)
+import Control.Monad.Eff.Console (CONSOLE(), log)
+import Control.Monad.Eff.Exception (EXCEPTION(), throwException)
 import Control.Monad.Eff.Console.Unsafe (logAny)
 import Control.Monad.Eff.Var (($=))
-import Control.Monad.Eff.Var as Var
-import Control.Monad.Free (foldFree, liftF, Free())
 
 import Data.Array (snoc)
-import Data.Either (Either(..))
 import Data.Foldable (foldMap)
-import Data.Foreign.Class (readProp)
-import Data.Functor (($>))
-import Data.Maybe (Maybe(..), isJust)
+import Data.Maybe (Maybe(..), isJust, isNothing)
 import Data.String (joinWith)
-import Data.Tuple (Tuple(..))
 
 import Halogen
-import Halogen.Util (appendToBody, onLoad)
 import Halogen.HTML.Indexed as H
 import Halogen.HTML.Events.Indexed as E
 import Halogen.HTML.Properties.Indexed as P
+import Halogen.Util (appendToBody, onLoad)
 
 import Network.HTTP.Affjax (AJAX(), post)
 
@@ -55,13 +48,11 @@ initialState = { messages: []
                , buffer: ""
                , user: "AnonymousCoward"
                , socket: Nothing
-               -- XXX skeevy placeholder
-               , connect: const (pure unit)
+               , connect: const $ log "ERROR: skeevy placeholder for connect was called somehow!"
                }
 
-type ChatMessage = { from :: User
-                   , content :: String
-                   -- , timeStamp :: hmm, so how do we do time in purescript?
+type ChatMessage = { content :: String
+                   -- a real app would probably store other stuff here
                    }
 type User = String
 
@@ -76,7 +67,7 @@ type AppEffects eff =
 
 -- | The component query algebra.
 data Query a
-  = ReceivedMessage User String a
+  = ReceivedMessage String a
   | SendMessage String a
   | SetBuffer String a
   | SetUserName String a
@@ -84,7 +75,7 @@ data Query a
   | Connect Connection a
   | Disconnect a
 
-
+-- | Didn't seem worth an extra bower import
 unlines :: Array String -> String
 unlines = joinWith "\n"
 
@@ -95,9 +86,8 @@ ui = component render eval
   where
     render :: State -> ComponentHTML Query
     render st =
-        H.div_ $
-            [ H.h1_
-                [ H.text "Silly Chat Thing" ]
+        H.div_
+            [ H.h1_ [ H.text "Silly Chat Thing" ]
             , H.p_
                 [ H.text "Username"
                 , H.input
@@ -107,22 +97,28 @@ ui = component render eval
                     ]
                 , H.button
                     [ E.onClick (E.input_ ConnectButton) ]
-                    [ H.text if isJust st.socket then "Disconnect" else "Connect" ]
+                    [ H.text if isJust st.socket
+                        then "Disconnect"
+                        else "Connect"
+                    ]
                 ]
             , H.p_
                 [ H.textarea
-                    [ P.value $ unlines $ map (\m -> m.from <> ": " <> m.content) st.messages ]
+                    [ P.value $ unlines $ map _.content st.messages
+                    , P.readonly true
+                    , P.class_ $ H.className "chatbox"
+                    ]
                 ]
             , H.p_
                 [ H.input
                     [ P.inputType P.InputText
+                    , P.class_ $ H.className "sendbuffer"
                     , P.placeholder "Type a message to send"
                     , P.value st.buffer
                     , E.onValueChange (E.input SetBuffer)
-                    -- , E.onValueInput (E.input SetTopic)
                     ]
                 , H.button
-                    [ P.disabled (not $ isJust st.socket)
+                    [ P.disabled (isNothing st.socket)
                     , E.onClick (E.input_ (SendMessage st.buffer))
                     ]
                     [ H.text "Send" ]
@@ -131,9 +127,6 @@ ui = component render eval
 
     eval :: Natural Query (ComponentDSL State Query (Aff (AppEffects ())))
     eval (ConnectButton next) = do
-        -- crap, how do i get hold of the driver in here?
-        -- could send it in with a query, but that feels skeevy
-        -- maybe we need to use AVars so we're less tightly coupled?
         connect <- gets _.connect
         liftEff' $ connect chatServerUrl
         pure next
@@ -147,13 +140,11 @@ ui = component render eval
         modify _ { socket = Nothing }
         liftAff' $ log' "lost the connection."
         pure next
-    eval (ReceivedMessage from content next) = do
-        modify \st -> st { messages = st.messages `snoc` {from: from, content: content}}
+    eval (ReceivedMessage content next) = do
+        modify \st -> st { messages = st.messages `snoc` {content: content}}
         pure next
     eval (SendMessage content next) = do
-        modify \st -> st { -- messages = st.messages `snoc` {from: st.user, content: content} , 
-                            buffer = ""
-                         }
+        modify _ { buffer = "" }
         gets _.socket >>= send' content
         pure next
     eval (SetBuffer content next) = do
@@ -163,20 +154,29 @@ ui = component render eval
         modify _ { user = user }
         pure next
 
--- this takes a Maybe because, well, that's how it is in State and it's more
--- convenient to keep the pattern match in one place
+-- | convencience fn to send a string through the websocket Connection.
+-- | this takes a Maybe because, well, that's how it is in State and it's more
+-- | convenient to keep the pattern match in one place. 
+-- | same goes for the liftEff, and even the argument order (for >>=).
 -- send :: String -> Maybe Connection -> Aff (ws :: WEBSOCKET | ()) Unit
 send :: String -> Maybe Connection -> Aff (AppEffects ()) Unit
 send _ Nothing                    = pure unit
 send s (Just (Connection socket)) = liftEff $ socket.send $ Message s
 
+-- | This is an even *more* convenient send helper that is hoisted into the lofty
+-- | hights of the Halogen Free Monad!
 -- send' :: forall eff. String -> Maybe Connection -> Free (HalogenF State Query (Aff (ws :: WEBSOCKET | eff)) Unit)
-send' :: String -> Maybe Connection -> Free (HalogenF State Query (Aff (AppEffects ()))) Unit
+send' :: String -> Maybe Connection -> ComponentDSL State Query (Aff (AppEffects ())) Unit
 send' s c = liftAff' $ send s c
 
+-- | saves us some typing and lift-spam
 log' :: forall eff. String -> Aff (console :: CONSOLE | eff) Unit
 log' = liftEff <<< log
 
+-- | Construct a WebSocket Connection and fill it out with the event handlers
+-- | we need for this particular app. The event handlers publish Query values
+-- | to the AVar argument, so they can be fed to the component's driver function
+-- | elsewhere.
 -- makeSocket :: forall eff. AVar (Query Unit) -> URL -> Eff (AppEffects eff) Unit
 makeSocket :: AVar (Query Unit) -> URL -> Eff (AppEffects ()) Unit
 makeSocket chan url = do
@@ -185,15 +185,13 @@ makeSocket chan url = do
     socket.onopen $= \event -> do
         logAny event
         log "onopen: Connection opened"
-        -- launchAff $ driver $ action $ Connect conn
         launchAff $ putVar chan $ action $ Connect conn
 
     socket.onmessage $= \event -> do
         logAny event
         let received = runMessage (runMessageEvent event)
         log $ "onmessage: Received '" ++ received ++ "'"
-        -- launchAff $ driver $ action $ ReceivedMessage "Remote" received
-        launchAff $ putVar chan $ action $ ReceivedMessage "Remote" received
+        launchAff $ putVar chan $ action $ ReceivedMessage received
 
     socket.onclose $= \event -> do
         logAny event
@@ -203,21 +201,12 @@ makeSocket chan url = do
     pure unit
 
 
--- | Run the app.
+-- | Spawn our various async doodaddery and go!
 main :: Eff (AppEffects ()) Unit
 main = do
     runAff throwException (const (pure unit)) $ do
-        log' "starting the app"
         chan <- makeVar
         app <- runUI ui (initialState { connect = makeSocket chan })
-        log' "setting the node to get appended onLoad"
         onLoad $ appendToBody app.node
-
-        -- li'l async event loop to forward from this socket AVar into the UI component
-        -- without either having to directly know about each other
-        forkAff $ forever do
-            log' "taking from the chan to feed the driver..."
-            (takeVar chan >>= app.driver)
-
-        log' "all done in main"
-
+        -- forever feed the beast
+        forever (takeVar chan >>= app.driver)
