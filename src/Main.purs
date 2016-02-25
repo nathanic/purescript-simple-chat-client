62284c16df2dd60b8c74ebada072d8ff37a0a09d
Nathan Stien <nathanism@gmail.com>
2016-02-24 22:07:19 -0600

    so, it "works" now, but i did some things i'm not proud of.
    i had a hell of a time making the websocket callbacks able to use the
    Driver to send queries into the component.  it worked fine when i was
    creating a single socket in main and i could just make closers over the
    driver; the problem came in when i wanted to be able to make a websocket
    in response to a button click, handled in the component's eval fn.
    
    halogen has been fun, but i struggled way too long for to figure
    out a workable hack. first, i tried sending the driver fn in with a new
    Query constructor called UncleanDriverHack.  i spent a whole evening
    trying to get this to typecheck, because it required that my State and
    my Query types have `forall eff` on a member field.
    
    then i tried another tactic: set up an AVar that the websocket callbacks
    use to publish Query values, and have a little forkAff in main that just
    feeds those into the driver fn.  i partially apply makeSocket to this,
    and then store that closure in the State when calling runUI.  at least
    this way the AVar is confined to main and the parameter to makeSocket..
    
    this time the problems manifested in different ways, but i think my
    problems sprang from the same issue with trying to have polymorphic eff
    vars down inside structures.
    
    does that make them existentials?  i'm a bit fuzzy on those.  it
    certainly seems like those State and Query eff type vars never wanted to
    unify with the other polymorphic effs from my other functions.
    
    in some cases it meant i'd end up with missing instances like:
    
        MonadEff (some effects...|e1) (Aff (superset of effects...|e2)
    
    i'd think e1 would just subsume the extra effects and this would
    typecheck, but nope.  sometimes this would come up with the EXACT same
    effects list but the polymorphic open row remainders wouldn't unify with
    each other.  i guess i don't grok purescript well enough yet.
    
    my final solution was the AVar thing plus inelegantly overinclusive
    closed rows for all my effects.  Everything's in Eff or Aff (AppEffects ()).
    i don't like this, but it finally built.  after my protracted quarrel with
    the compiler, it all worked perfectly at runtime.

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,21 @@
 module Main where
 
--- check out https://github.com/parsonsmatt/purs-architecture-tutorial/
-
 import Prelude
 
 import Control.Alt ((<|>))
-import Control.Monad.Aff (Aff(), runAff)
-import Control.Monad.Aff.Console (log)
+import Control.Bind ((=<<))
+import Control.Monad (when)
+import Control.Monad.Rec.Class (forever)
+import Control.Monad.Aff (Aff(),launchAff,runAff,forkAff)
+import Control.Monad.Aff.AVar (AVar(),AVAR(),makeVar,putVar,takeVar)
+-- import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE())
-import Control.Monad.Eff.Exception (throwException)
+import Control.Monad.Eff.Console (CONSOLE(),log)
+import Control.Monad.Eff.Exception (EXCEPTION(),throwException)
+import Control.Monad.Eff.Console.Unsafe (logAny)
+import Control.Monad.Eff.Var (($=))
+import Control.Monad.Eff.Var as Var
 import Control.Monad.Free (foldFree, liftF, Free())
 
 import Data.Array (snoc)
@@ -18,7 +23,7 @@ import Data.Either (Either(..))
 import Data.Foldable (foldMap)
 import Data.Foreign.Class (readProp)
 import Data.Functor (($>))
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), isJust)
 import Data.String (joinWith)
 import Data.Tuple (Tuple(..))
 
@@ -30,59 +35,62 @@ import Halogen.HTML.Properties.Indexed as P
 
 import Network.HTTP.Affjax (AJAX(), post)
 
+import WebSocket
+
+-- | hard-coded because this is just a dang demo
+chatServerUrl :: URL
+chatServerUrl = URL "ws://localhost:9160"
 
 -- | The state of the component.
-type State = { messages :: Array Message
+type State = { messages :: Array ChatMessage
              , buffer :: String
-             , connected :: Boolean
              , user :: User
+             , socket :: Maybe Connection
+             -- , connect :: forall eff. URL -> Eff (AppEffects eff) Unit
+             , connect :: URL -> Eff (AppEffects ()) Unit
              }
 
 initialState :: State
 initialState = { messages: []
                , buffer: ""
-               , connected: false
-               , user: "Joe Bob"
+               , user: "AnonymousCoward"
+               , socket: Nothing
+               -- XXX skeevy placeholder
+               , connect: const (pure unit)
                }
 
-type Message = { from :: User
-               , content :: String
-               -- , timeStamp :: hmm, so how do we do time in purescript?
-               }
+type ChatMessage = { from :: User
+                   , content :: String
+                   -- , timeStamp :: hmm, so how do we do time in purescript?
+                   }
 type User = String
+
+-- | The effects used in the app.
+type AppEffects eff =
+        HalogenEffects (
+              -- ajax :: AJAX ,
+              console :: CONSOLE,
+              ws :: WEBSOCKET
+            | eff
+            )
 
 -- | The component query algebra.
 data Query a
   = ReceivedMessage User String a
   | SendMessage String a
   | SetBuffer String a
-  | Connect a
+  | SetUserName String a
+  | ConnectButton a
+  | Connect Connection a
   | Disconnect a
 
--- | Output algebra, which the component sends to the effectful stuff at the bottom
-data OutputF a = Log String a
-               | SendToServer String a
-type Output = Free OutputF
-
-output :: String -> Output Unit
-output msg = liftF (Log msg unit)
-
-send :: String -> Output Unit
-send msg = liftF (SendToServer msg unit)
-
--- | The effects used in the app.
-type AppEffects eff = 
-        HalogenEffects (
-              -- ajax :: AJAX , 
-              console :: CONSOLE 
-            | eff
-            )
 
 unlines :: Array String -> String
 unlines = joinWith "\n"
 
 -- | The definition for the app's main UI component.
-ui :: Component State Query Output
+-- ui :: forall eff. Component State Query (Aff (AppEffects eff))
+ui :: Component State Query (Aff (AppEffects ()))
 ui = component render eval
   where
     render :: State -> ComponentHTML Query
@@ -90,74 +98,126 @@ ui = component render eval
         H.div_ $
             [ H.h1_
                 [ H.text "Silly Chat Thing" ]
-            , H.p_ 
+            , H.p_
+                [ H.text "Username"
+                , H.input
+                    [ P.inputType P.InputText
+                    , P.value st.user
+                    , E.onValueChange (E.input SetUserName)
+                    ]
+                , H.button
+                    [ E.onClick (E.input_ ConnectButton) ]
+                    [ H.text if isJust st.socket then "Disconnect" else "Connect" ]
+                ]
+            , H.p_
                 [ H.textarea
                     [ P.value $ unlines $ map (\m -> m.from <> ": " <> m.content) st.messages ]
                 ]
             , H.p_
-                [ H.input 
-                    [ P.inputType P.InputText 
+                [ H.input
+                    [ P.inputType P.InputText
                     , P.placeholder "Type a message to send"
                     , P.value st.buffer
                     , E.onValueChange (E.input SetBuffer)
                     -- , E.onValueInput (E.input SetTopic)
                     ]
                 , H.button
-                    [ P.disabled (not st.connected)
+                    [ P.disabled (not $ isJust st.socket)
                     , E.onClick (E.input_ (SendMessage st.buffer))
                     ]
                     [ H.text "Send" ]
                 ]
             ]
 
-    eval :: Natural Query (ComponentDSL State Query Output)
-    eval (Connect next) = do
-        modify _ { connected = true }
-        liftH $ output "got a connection!"
+    eval :: Natural Query (ComponentDSL State Query (Aff (AppEffects ())))
+    eval (ConnectButton next) = do
+        -- crap, how do i get hold of the driver in here?
+        -- could send it in with a query, but that feels skeevy
+        -- maybe we need to use AVars so we're less tightly coupled?
+        connect <- gets _.connect
+        liftEff' $ connect chatServerUrl
+        pure next
+    eval (Connect conn next) = do
+        modify _ { socket = Just conn }
+        liftAff' $ log' "got a connection!"
+        st <- get
+        send' ("Hi! I am " <> st.user) st.socket
         pure next
     eval (Disconnect next) = do
-        modify _ { connected = false }
-        liftH $ output "lost the connection."
+        modify _ { socket = Nothing }
+        liftAff' $ log' "lost the connection."
         pure next
     eval (ReceivedMessage from content next) = do
         modify \st -> st { messages = st.messages `snoc` {from: from, content: content}}
         pure next
     eval (SendMessage content next) = do
-        modify \st -> st { messages = st.messages `snoc` {from: st.user, content: content}
-                         , buffer = ""
+        modify \st -> st { -- messages = st.messages `snoc` {from: st.user, content: content} , 
+                            buffer = ""
                          }
-        liftH $ send content
+        gets _.socket >>= send' content
         pure next
     eval (SetBuffer content next) = do
         modify _ { buffer = content }
         pure next
+    eval (SetUserName user next) = do
+        modify _ { user = user }
+        pure next
 
-ui' :: forall eff. Component State Query (Aff (AppEffects eff))
-ui' = interpret (foldFree evalOutput) ui
-  where
-  evalOutput :: Natural OutputF (Aff (AppEffects eff))
-  evalOutput (Log msg next) = do
-    log msg
-    pure next
-  evalOutput (SendToServer msg next) = do
-    log $ "sending to server: " <> msg
-    pure next
+-- this takes a Maybe because, well, that's how it is in State and it's more
+-- convenient to keep the pattern match in one place
+-- send :: String -> Maybe Connection -> Aff (ws :: WEBSOCKET | ()) Unit
+send :: String -> Maybe Connection -> Aff (AppEffects ()) Unit
+send _ Nothing                    = pure unit
+send s (Just (Connection socket)) = liftEff $ socket.send $ Message s
+
+-- send' :: forall eff. String -> Maybe Connection -> Free (HalogenF State Query (Aff (ws :: WEBSOCKET | eff)) Unit)
+send' :: String -> Maybe Connection -> Free (HalogenF State Query (Aff (AppEffects ()))) Unit
+send' s c = liftAff' $ send s c
+
+log' :: forall eff. String -> Aff (console :: CONSOLE | eff) Unit
+log' = liftEff <<< log
+
+-- makeSocket :: forall eff. AVar (Query Unit) -> URL -> Eff (AppEffects eff) Unit
+makeSocket :: AVar (Query Unit) -> URL -> Eff (AppEffects ()) Unit
+makeSocket chan url = do
+    conn@(Connection socket) <- newWebSocket url []
+
+    socket.onopen $= \event -> do
+        logAny event
+        log "onopen: Connection opened"
+        -- launchAff $ driver $ action $ Connect conn
+        launchAff $ putVar chan $ action $ Connect conn
+
+    socket.onmessage $= \event -> do
+        logAny event
+        let received = runMessage (runMessageEvent event)
+        log $ "onmessage: Received '" ++ received ++ "'"
+        -- launchAff $ driver $ action $ ReceivedMessage "Remote" received
+        launchAff $ putVar chan $ action $ ReceivedMessage "Remote" received
+
+    socket.onclose $= \event -> do
+        logAny event
+        log "onclose: Connection closed"
+        launchAff $ putVar chan $ action $ Disconnect
+
+    pure unit
+
 
 -- | Run the app.
 main :: Eff (AppEffects ()) Unit
-main = runAff throwException (const (pure unit)) $ do
-  log "starting the app"
-  app <- runUI ui' initialState
-  log "setting the node to get appended onLoad"
-  onLoad $ appendToBody app.node
+main = do
+    runAff throwException (const (pure unit)) $ do
+        log' "starting the app"
+        chan <- makeVar
+        app <- runUI ui (initialState { connect = makeSocket chan })
+        log' "setting the node to get appended onLoad"
+        onLoad $ appendToBody app.node
 
-  -- connect websocket
-  -- use the driver to send in events
-  -- but how will the app send events out?
-  -- what about that "interpret" example?
-  -- https://github.com/aphorisme/haskell-socketson
-  log "sending some fake driver messages"
-  app.driver $ Connect unit
-  app.driver $ ReceivedMessage "Nobody" "What's up, doc?" unit
-  log "all done in main"
+        -- li'l async event loop to forward from this socket AVar into the UI component
+        -- without either having to directly know about each other
+        forkAff $ forever do
+            log' "taking from the chan to feed the driver..."
+            (takeVar chan >>= app.driver)
+
+        log' "all done in main"
 
